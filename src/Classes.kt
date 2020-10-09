485cff8250d7a34f09c89fafc9b91104c78e67c1
PetePrattis <panagiotispr1997@gmail.com>
2020-10-09 15:13:41 +0300

    classes & inheritance

--- src/Classes.kt
+++ src/Classes.kt
@@ -1,26 +1,51 @@
-class Car (val make: String, val model: String){
+open class Vehicle(val make: String, val model: String){
 
-    fun accelerate(){
-        println("faster")
+    open fun accelerate(){
+        println("$model go faster")
     }
 
-}
-
-class Truck (val make: String, val model: String, val weigt: Int){
-
-    fun tow(){
-        println("heavy")
+    fun brake(){
+        println("$make go slower")
     }
 }
+
+class Car(make: String, model: String, var color: String) : Vehicle(make, model){
+    override fun accelerate(){
+        println("$model go even faster")
+        super.accelerate()
+    }
+}
+
+class Truck(make: String, model: String, var weight: Int): Vehicle(make, model)
+
+//class Car (val make: String, val model: String){
+//
+//    fun accelerate(){
+//        println("faster")
+//    }
+//
+//}
+//
+//class Truck (val make: String, val model: String, val weigt: Int){
+//
+//    fun tow(){
+//        println("heavy")
+//    }
+//}
 
 fun main(){
 
-    val car = Car("Honda","Jazz")
-    println(car.make +" "+car.model)
-    car.accelerate()
+//    val car = Car("Honda","Jazz")
+//    println(car.make +" "+car.model)
+//    car.accelerate()
+//
+//    val truck = Truck("Ford","F-150",10000)
+//    truck.tow()
+
+    val tesla = Car("Tesla","ModelS","Red")
+    tesla.accelerate()
 
     val truck = Truck("Ford","F-150",10000)
-    truck.tow()
-
+    truck.accelerate()
 
 }
