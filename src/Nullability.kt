ea7485009e0f326347d0f5f2053652ef1bd0a73a
PetePrattis <panagiotispr1997@gmail.com>
2020-10-09 14:53:24 +0300

    classes

--- src/Nullability.kt
+++ src/Nullability.kt
@@ -3,34 +3,38 @@ fun main() {
     var name : String = "Pete"
     //name = null
 
-    // !!
     var a = null
     var nullableV : String? = "Maybe I exist"
+    //nullableV = null
 
     //println(nullableV.length)
 
     //Null check
     var length = 0
-    if (nullableV != null)
+    if (nullableV != null) {
         length = nullableV.length
-    else
+        println(nullableV.length)
+    }
+    else{
         length = -1
+        println(length)
+    }
 
-    println(length)
 
     val l = if (nullableV != null) nullableV.length else -1
 
     //safe call operator
+    nullableV = null
     println(nullableV?.length)
 
     //elvis operator
     val len = nullableV?.length ?: -1
     val newn = nullableV ?: "No one"
 
-    println(newn)
+    println("$len $newn")
 
     //nullableV = null
     // !!
-    println(nullableV!!.length)
+    //println(nullableV!!.length)
 
 }
